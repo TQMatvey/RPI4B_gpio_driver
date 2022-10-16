fa273aa2be2721520d4a74e7dea5eb40c1548419
TQMatvey <maybeetube@gmail.com>
2022-10-16 20:22:20 +0700

    fix bash script

--- led.sh
+++ led.sh
@@ -1,7 +1,7 @@
 while [ 1 == 1 ]
 do
   echo "26,1" > /proc/gpio_control
-  sleep 2
-  echo "26,1" > /proc/gpio_control
-  sleep 2
+  sleep 0.2
+  echo "26,0" > /proc/gpio_control
+  sleep 0.2
 done
